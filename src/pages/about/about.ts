b770877f8769cfa2dfcc3391068203b1c518e572
Joao Carlos Matsuzaka Costa <jc@turntable.local>
2017-04-28 11:36:51 -0400

    added observable async variable on about

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { MqttService, MqttMessage } from "ngx-mqtt";
 import { NavController, Tabs } from "ionic-angular";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  tabs: Tabs;
+  observableMessage: Observable<MqttMessage>;
+  private tabs: Tabs;
+  topic = 'other';
 
   constructor(private _mqttService: MqttService, private navCtrl: NavController) {
     this.tabs = navCtrl.parent;
@@ -17,7 +20,7 @@ export class AboutPage {
     console.log('ionViewDidLoad AboutPage');
 
     this._mqttService.observe('about')
-        .subscribe((message: MqttMessage) => {
+      .subscribe((message: MqttMessage) => {
           let msg = message.payload.toString();
           console.log(msg);
           if(msg == 'home') {
@@ -28,7 +31,14 @@ export class AboutPage {
             console.log('changing tab to contact');
             this.tabs.select(2);
           }
-        });
+    });
+    this.observableMessage = this._mqttService.observe(this.topic);
+        
+  }
+
+  onTopicChange(newtopic: string) {
+    console.log('onTopicChange to ' + newtopic)
+    this.observableMessage = this._mqttService.observe(newtopic);
   }
 
 }
